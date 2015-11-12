fb884976fd916e1da21593108107ca5058ce2c3d
fiiidget <shackney@pratt.edu>
2015-11-12 12:40:48 -0500

    adding a counting function

--- metbbscrape.py
+++ metbbscrape.py
@@ -29,18 +29,25 @@ for a_image in player_image:
     #     print(card["alt"])
     #     print(card["src"])
 
-    if cardimg["alt"] not  in card_alt_text:
+    Alt_text = cardimg["alt"]
+    Image_file = cardimg["src"]
+
+    if Alt_text not  in card_alt_text:
         card_alt_text.append("alt")
 
-    if cardimg["src"] not in card_images:
+    if Image_file not in card_images:
         card_images.append("src")
 
-print(cardimg["alt"])
-print(cardimg["src"])
+print(Alt_text)
+print(Image_file)
 
 def cardlist(listo):
     totalcards = len(listo)
-    print("you have" + totalcards + "in your list")
+
+    answer = ("you have " + str(totalcards) + " items in your " + str(listo) + " list")
+    return answer
 
 number_of_cards = cardlist(card_images)
+numbers_of_descriptions = cardlist(card_alt_text)
 print(number_of_cards)
+print(numbers_of_descriptions)
