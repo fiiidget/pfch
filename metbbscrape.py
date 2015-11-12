107c2062558dd6ac810756f1b57d9f508df9b926
fiiidget <shackney@pratt.edu>
2015-11-12 15:57:18 -0500

    solving list name and counting problem

--- metbbscrape.py
+++ metbbscrape.py
@@ -1,10 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
 
-cardpages = []
+cardpages = ["pages"]
 cards = {}
-card_images = []
-card_alt_text = []
+card_images = ["images"]
+card_alt_text = ["alt text"]
 
 
 url = "http://www.metmuseum.org/collection/the-collection-online/search/413556?pos=1&rpp=90&pg=1&ao=on&ft=baseball+cards"
@@ -35,18 +35,18 @@ for a_image in player_image:
     Image_file = cardimg["src"]
 
     if Alt_text not in card_alt_text:
-        card_alt_text.append("alt")
+        card_alt_text.append(Alt_text)
 
     if Image_file not in card_images:
-        card_images.append("src")
+        card_images.append(Image_file)
 
 print(Alt_text)
 print(Image_file)
 
 def cardlist(listo):
-    totalcards = len(listo)
+    totalcards = (len(listo)) - 1
 
-    answer = ("you have " + str(totalcards) + " items in your " + str(listo) + " list")
+    answer = ("you have " + str(totalcards) + " real items in your " + str(listo[0]) + " list")
     return answer
 
 number_of_cards = cardlist(card_images)
@@ -56,4 +56,10 @@ print(numbers_of_descriptions)
 
 next_link = soup.find("a", attrs = {"class" : "next"})
 
-print(next_link["href"])
+if next_link["href"] not in cardpages:
+    cardpages.append(next_link["href"])
+
+
+print(cardpages)
+print(cardlist(cardpages))
+print(card_images)
